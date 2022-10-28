1a22bd4c015c1b1f006c5067812decc3b4d544c1
mariaka86 <marymwihaki123@gmail.com>
2022-10-27 20:10:48 -0500

    got text area to show

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -6,16 +6,15 @@ function Form (props) {
   
   const [method, setMethod] = useState('')
   const [url,setUrl] =useState('https://pokeapi.co/api/v2/pokemon')
+  const[JSON,setJSON]= useState();
 
   const handleSubmit = e => {
     e.preventDefault();
    
-    props.handleApiCall({method,url})
+    props.handleApiCall({method,url,JSON})
   }
  
-  const handleMethod = (e) =>{
-    setMethod(e.target.value)
-  }
+ 
 
     return (
       <>
@@ -25,11 +24,14 @@ function Form (props) {
             <input onChange = {(e)=>setUrl(e.target.value)} name='url' type='text' />
             <button type="submit">GO!</button>
           </label>
+
+          <textarea className ="text-input"onChange = {(e)=>setJSON(e.target.value)} placeholder='Put/Post input' type='text'/>
+          
           <label className="methods">
-            <span id="get" onClick={handleMethod}>GET</span>
-            <span id="post" onClick={handleMethod}>POST</span>
-            <span id="put" onClick={handleMethod}>PUT</span>
-            <span id="delete" onClick={handleMethod}>DELETE</span>
+            <span id="get" onClick={(e)=> setMethod('get')}>GET</span>
+            <span id="post" onClick={(e)=> setMethod('post')}>POST</span>
+            <span id="put" onClick={(e)=> setMethod('put')}>PUT</span>
+            <span id="delete" onClick={(e)=> setMethod('delete')}>DELETE</span>
           </label>
         </form>
       </>
